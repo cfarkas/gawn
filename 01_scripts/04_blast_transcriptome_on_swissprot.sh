bae31172588a48d886a44acb053175b45cef80d1
cfarkas <carlosfarkas@gmail.com>
2021-02-10 20:14:12 -0600

    Update 04_blast_transcriptome_on_swissprot.sh

--- 01_scripts/04_blast_transcriptome_on_swissprot.sh
+++ 01_scripts/04_blast_transcriptome_on_swissprot.sh
@@ -15,7 +15,7 @@ SWISSPROT_HITS="$BASENAME".hits
 # Blast all sequences against local swissprot database
 cat "$DATA_FOLDER"/"$TRANSCRIPTOME_NAME" |
     parallel -j "$NCPUS" -k --block 1k --recstart '>' --pipe 'blastx -db '$SWISSPROT_DB' \
-        -query - -evalue 1e-10 -qcov_hsp_perc 20 -outfmt 6 -max_target_seqs 1' > "$SWISSPROT_RESULTS"
+        -query - -evalue 1e-10 -qcov_hsp_perc 10 -outfmt 6 -max_target_seqs 1' > "$SWISSPROT_RESULTS"
 
 # Extract analyzed_genes.hits
 awk '{print $1,$2}' "$SWISSPROT_RESULTS" | uniq > "$SWISSPROT_HITS"
